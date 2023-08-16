d7256bb4f54f3a98f0c4a77da17538a183abeac9
unknown <brandon_mark_cantos@dlsl.edu.ph>
2023-08-16 22:34:15 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dist/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        puti: '#ffffff',
+        medyoputi: '#FDFAF1',
+        choco: '#703412',
+        berde: '#084935',
+        medyoblack: '#2D2C2C',
+        medyoblack2: '#515050',
+        kape: '#CBBCA9',
+        mangga: '#F5D087',
+        maitim: '#000000'
+      }
+    },
+  },
+  plugins: [],
+}
+
